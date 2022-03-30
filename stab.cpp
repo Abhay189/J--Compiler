2efbc97f5f8da4a8bd3b78682c096ba6993565ac
Abhay189 <sabhay2001@gmail.com>
2022-03-30 03:08:25 -0600

    adding second pass completed

--- stab.cpp
+++ stab.cpp
@@ -1,7 +1,9 @@
-#include"stab.hpp"
-#include<string>
-#include<iostream>
+#include "stab.hpp"
+#include <string>
+#include <iostream>
 #include <vector>
+#include <bits/stdc++.h>
+#include <tuple> 
 
 /**
  * Called to iterate over all the global declerations.  
@@ -15,6 +17,154 @@ int main_declaration_counter = 0;
  * 
  */
 std::vector<std::unordered_map<std::string, std::string> *> scopeStack;
+
+std::vector<std::vector<NodeType>> OperatorTypeChecker(std::string operatorType){
+    std::vector<std::vector<NodeType>> pp;
+    if(operatorType == "||"){
+        std::vector<NodeType> p; 
+        p.push_back(NodeType::BOOL_TYPE);
+        p.push_back(NodeType::BOOL_TYPE);
+        p.push_back(NodeType::BOOL_TYPE);
+        
+        pp.push_back(p);
+    }
+    else if(operatorType == "&&"){
+        std::vector<NodeType> p; 
+        p.push_back(NodeType::BOOL_TYPE);
+        p.push_back(NodeType::BOOL_TYPE);
+        p.push_back(NodeType::BOOL_TYPE);
+        
+        pp.push_back(p);
+    }
+    else if(operatorType == "=="){
+        std::vector<NodeType> p; 
+        p.push_back(NodeType::BOOL_TYPE);
+        p.push_back(NodeType::BOOL_TYPE);
+        p.push_back(NodeType::BOOL_TYPE);
+
+        std::vector<NodeType> p1; 
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::BOOL_TYPE);
+        
+        pp.push_back(p);
+        pp.push_back(p1);
+    }
+    else if(operatorType == "!="){
+        std::vector<NodeType> p; 
+        p.push_back(NodeType::BOOL_TYPE);
+        p.push_back(NodeType::BOOL_TYPE);
+        p.push_back(NodeType::BOOL_TYPE);
+
+        std::vector<NodeType> p1; 
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::BOOL_TYPE);
+        
+        pp.push_back(p);
+        pp.push_back(p1);
+    }
+    else if(operatorType == "="){
+        std::vector<NodeType> p; 
+        p.push_back(NodeType::BOOL_TYPE);
+        p.push_back(NodeType::BOOL_TYPE);
+        p.push_back(NodeType::BOOL_TYPE);
+
+        std::vector<NodeType> p1; 
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::INT_TYPE);
+        
+        pp.push_back(p);
+        pp.push_back(p1);
+    }
+    else if(operatorType == "<"){
+        std::vector<NodeType> p; 
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::BOOL_TYPE);
+        
+        pp.push_back(p);
+    }
+    else if(operatorType == ">"){
+        std::vector<NodeType> p; 
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::BOOL_TYPE);
+        
+        pp.push_back(p);
+    }
+    else if(operatorType == "<="){
+        std::vector<NodeType> p; 
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::BOOL_TYPE);
+        
+        pp.push_back(p);
+    }
+    else if(operatorType == ">="){
+        std::vector<NodeType> p; 
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::BOOL_TYPE);
+        
+        pp.push_back(p);
+    }
+    else if(operatorType == "+"){
+        std::vector<NodeType> p; 
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::INT_TYPE);
+        
+        pp.push_back(p);
+    }
+    else if(operatorType == "*"){
+        std::vector<NodeType> p; 
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::INT_TYPE);
+        
+        pp.push_back(p);
+    }
+    else if(operatorType == "/"){
+        std::vector<NodeType> p; 
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::INT_TYPE);
+        
+        pp.push_back(p);
+    }
+    else if(operatorType == "%"){
+        std::vector<NodeType> p; 
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::INT_TYPE);
+        
+        pp.push_back(p);
+    }
+    else if(operatorType == "!"){
+        std::vector<NodeType> p; 
+        p.push_back(NodeType::BOOL_TYPE);
+        p.push_back(NodeType::BOOL_TYPE);
+        
+        pp.push_back(p);
+    }
+    else if(operatorType == "-"){
+        std::vector<NodeType> p; 
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::INT_TYPE);
+
+        std::vector<NodeType> p1; 
+        p.push_back(NodeType::INT_TYPE);
+        p.push_back(NodeType::INT_TYPE);
+        
+        pp.push_back(p);
+        pp.push_back(p1);
+    }
+    return pp;
+
+}
 
 /**
  * @brief Looks up if an identifier has been defined in any stab in the stack scope. 
@@ -89,7 +239,7 @@ void Formals_String_Generator(std::string * thestring ,AstNode * Node, std::unor
             Node_stab->insert({var_Identifier,var_Info});
         }
         else{
-            std::cerr << "Multiple Declerations of a Variable \"" + a->ChildrenArray[1]->AstStringval + "\"\n";
+            std::cerr << "error: Multiple Declerations of a Variable \"" + a->ChildrenArray[1]->AstStringval + "\"\n";
             exit(EXIT_FAILURE);
         }
     }
@@ -122,7 +272,7 @@ void First_Iteration_Callback_Function(AstNode * Node, std::unordered_map<std::s
                 if(Node_stab->count(node_name) == 0){
                     Node_stab->insert({node_name,node_name});
                 }else{
-                    std::cerr << "More then one Main 5 declearation found !!\n";
+                    std::cerr << "error: More then one Main declearation found !!\n";
                     exit(EXIT_FAILURE);
                 }
 
@@ -154,7 +304,7 @@ void First_Iteration_Callback_Function(AstNode * Node, std::unordered_map<std::s
                     Node_stab->insert({Function_Identifier,Function_info});
                 }
                 else{
-                    std::cerr << "Multiple Declerations of a function \"" + Function_Identifier + "\"\n";
+                    std::cerr << "error: Multiple Declerations of a function \"" + Function_Identifier + "\"\n";
                     exit(EXIT_FAILURE);
                 }
                 break;
@@ -181,7 +331,7 @@ void First_Iteration_Callback_Function(AstNode * Node, std::unordered_map<std::s
                 Node_stab->insert({var_Identifier,var_Info});
             }
             else{
-                std::cerr << "Multiple Declerations of variable \"" + var_Identifier + "\" in global scope\n";
+                std::cerr << "error: Multiple Declerations of variable \"" + var_Identifier + "\" in global scope\n";
                 exit(EXIT_FAILURE);
             }
             break;
@@ -216,6 +366,27 @@ void secondIteration(AstNode * Rootnode){
     }
 }
 
+/**
+ * @brief This function is for splitting the inputted string into tokens and returning those tokens into a vector of strings. 
+ * 
+ * @param s : The input string to be tokenized
+ * @param option : there are two options - input 1 for decleration strings - 2 for function invocations.  
+ * @return std::vector<std::string> : Vector containing string tokens, splitted at whitespaces.
+ */
+std::vector<std::string> simple_tokenizer(std::string s, int option)
+{ 
+    std::vector<std::string> temp;
+    std::stringstream ss(s);
+    std::string word;
+    int count = 0;
+    while (ss >> word) {
+        if(count != 0 || option == 2){
+            temp.push_back(word);
+        }
+        count++;
+    }
+    return temp;
+}
 
 void Second_Iteration_Callback_Function(AstNode * Node, std::unordered_map<std::string, std::string> * Node_stab){
     switch(Node->AstNodeType){
@@ -243,22 +414,22 @@ void Second_Iteration_Callback_Function(AstNode * Node, std::unordered_map<std::
                     topStab->insert({var_Identifier,var_Info});
                     }
                     else{
-                        std::cerr << "Multiple Declerations of variable \"" + var_Identifier + "\" within the same scope\n";
+                        std::cerr << "error: Multiple Declerations of variable \"" + var_Identifier + "\" within the same scope\n";
                         exit(EXIT_FAILURE);
                     }
                 }
                 else{
-                    std::cerr << "Variable decleration not in outermost block \"" + var_Identifier + "\"\n";
+                    std::cerr << "error: Variable decleration not in outermost block \"" + var_Identifier + "\"\n";
                         exit(EXIT_FAILURE);
                 }
             }
             break;
         }
- 
+        
         case NodeType::ID:{
             auto node_stab_info = AstStackLookup(Node->AstStringval);
             if(node_stab_info == ""){
-                std::cerr << "Identifier not defined! \"" + Node->AstStringval + "\"\n";
+                std::cerr << "error: Identifier not defined! \"" + Node->AstStringval + "\"\n";
                 exit(EXIT_FAILURE);
             }
             break;
@@ -276,42 +447,61 @@ void Second_Iteration_Callback_Function(AstNode * Node, std::unordered_map<std::
                     case NodeType::STRING: {Function_parameters.append(" STRING"); break;}
                     case NodeType::TRUE: {Function_parameters.append(" BOOLEAN"); break;}
                     case NodeType::FALSE: {Function_parameters.append(" BOOLEAN"); break;}
-                    default: break;
+                    case NodeType::OPERATOR: {
+                        auto ppp = OperatorTypeChecker(a->AstStringval);
+                        for(auto a : ppp){
+                            if(a[2] == NodeType::BOOL_TYPE){
+                                Function_parameters.append(" BOOLEAN");
+                            }
+                            else if(a[2] == NodeType::INT_TYPE){
+                                Function_parameters.append(" INT");
+                            }
+                        }
+                        break;
+                    }
+                    case NodeType::UNARY_EXPRESSION: {
+                        auto ppp = OperatorTypeChecker(a->AstStringval);
+                        for(auto a : ppp){
+                            if(a[1] == NodeType::BOOL_TYPE){
+                                Function_parameters.append(" BOOLEAN");
+                            }
+                            else if(a[1] == NodeType::INT_TYPE){
+                                Function_parameters.append(" INT");
+                            }
+                        }
+                        break;
+                    }
+                    default: Function_parameters.append(" something"); break;
                 }
             }
             Function_info = Function_Identifier + Function_parameters;
 
             auto node_stab_info = AstStackLookup(Function_Identifier);
-            if(node_stab_info == ""){
-                std::cerr << "Function not defined before use! \"" + Function_Identifier + "\"\n";
+            if(node_stab_info == Function_Identifier){
+                std::cerr << "error: Main function called inside a function. \n";
                 exit(EXIT_FAILURE);
             }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            if(node_stab_info == ""){
+                std::cerr << "error: Function not defined before use! \"" + Function_Identifier + "\"\n";
+                exit(EXIT_FAILURE);
+            }
+            auto node_stab_info_vector = simple_tokenizer(node_stab_info,1);
+            auto function_info_vector = simple_tokenizer(Function_info,2);
             
-            // std::string delimiter = " ";
-            // std::string token = node_stab_info.substr(3, node_stab_info.find(delimiter));
-
-            std::cout<<node_stab_info<<std::endl;
-            std::cout<<Function_info<<std::endl;
+            //Checking is the function invocation is called with the right amount of parameters. 
+            if(function_info_vector.size() != node_stab_info_vector.size()){
+                std::cerr << "error: Number of parameter in the function invocation \"" + Function_Identifier + "\" are not equal to number of parameter in its decleration.\n";
+                exit(EXIT_FAILURE);
+            }
+            //To check if the function has been called with the right types of parameters. 
+            if(node_stab_info_vector != function_info_vector){
+                std::cerr << "error: Function \"" + Function_Identifier + "\" is called with wrong type(s) of parameter.\n";
+                exit(EXIT_FAILURE);
+            }
             break;
         }
         
         default: break;
     }
 }
+
